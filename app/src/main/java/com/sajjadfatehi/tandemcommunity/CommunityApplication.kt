0ddc15191b47d552c010c241cc14cc22afc142b0
sajjadfatehi <sajad.fatehi@snapp.cab>
2026-06-14 15:32:08 +0330

    Add base Application class and add it to manifest for hilt setup

---
+++ app/src/main/java/com/sajjadfatehi/tandemcommunity/CommunityApplication.kt
@@ -0,0 +1,8 @@
+package com.sajjadfatehi.tandemcommunity
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CommunityApplication : Application() {
+}
---
+++ app/src/main/java/com/sajjadfatehi/tandemcommunity/di/CommunityModule.kt
@@ -0,0 +1,10 @@
+package com.sajjadfatehi.tandemcommunity.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class CommunityModule {
+}
